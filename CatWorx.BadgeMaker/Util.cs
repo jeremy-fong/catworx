7e551e908f9165b24db9aea0b98d0c63312cd26b
jeremy-fong <112743562+jeremy-fong@users.noreply.github.com>
2023-07-25 19:24:10 -0700

    added code to insert image onto the badge template

--- CatWorx.BadgeMaker/Util.cs
+++ CatWorx.BadgeMaker/Util.cs
@@ -2,7 +2,9 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Net.Http;
 using SkiaSharp;
+using System.Threading.Tasks;
 
 namespace CatWorx.BadgeMaker
 {
@@ -25,7 +27,7 @@ namespace CatWorx.BadgeMaker
       // If not, create it
       Directory.CreateDirectory("data");
       }
-      using (StreamWriter file = new StreamWriter("data/employees.csv"))
+      using(StreamWriter file = new StreamWriter("data/employees.csv"))
       {
         file.WriteLine("ID,Name,PhotoUrl");
         // Loop over employees
@@ -37,12 +39,35 @@ namespace CatWorx.BadgeMaker
         }
       }
     }
-    public static void MakeBadges(List<Employee> employees)
+    async public static Task MakeBadges(List<Employee> employees)
     {
-       // Create image
-       SKImage newImage = SKImage.FromEncodedData(File.OpenRead("badge.png"));
-       SKData data = newImage.Encode();
-       data.SaveTo(File.OpenWrite("data/employeeBadge.png"));
+        // Layout variables
+        int BADGE_WIDTH = 669;
+        int BADGE_HEIGHT = 1044;
+
+        int PHOTO_LEFT_X = 184;
+        int PHOTO_TOP_Y = 215;
+        int PHOTO_RIGHT_X = 486;
+        int PHOTO_BOTTOM_Y = 517;
+       // instance of HttpClient is disposed after code in the block has run
+       using(HttpClient client = new HttpClient())
+       {
+        for (int i = 0; i < employees.Count; i++)
+        {
+            SKImage photo = SKImage.FromEncodedData(await client.GetStreamAsync(employees[i].GetPhotoUrl()));
+            SKImage background = SKImage.FromEncodedData(File.OpenRead("badge.png"));
+
+            SKBitmap badge = new SKBitmap(BADGE_WIDTH, BADGE_HEIGHT);
+            SKCanvas canvas = new SKCanvas(badge);
+
+            canvas.DrawImage(background, new SKRect(0, 0, BADGE_WIDTH, BADGE_HEIGHT));
+            canvas.DrawImage(photo, new SKRect(PHOTO_LEFT_X, PHOTO_TOP_Y, PHOTO_RIGHT_X, PHOTO_BOTTOM_Y));
+
+            SKImage finalImage = SKImage.FromBitmap(badge);
+            SKData data = finalImage.Encode();
+            data.SaveTo(File.OpenWrite("data/employeeBadge.png"));
+        }
+       }
     }
   }
 }
